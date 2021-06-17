7bdfa6dee14d12c72b9a355c9548054268af5e66
walissonfederal <diretor@federalsistemas.com.br>
2021-06-17 01:45:20 -0300

    feito a deleção dos perfumes do carrinho

--- resources/views/global/javascript.blade.php
+++ resources/views/global/javascript.blade.php
@@ -13,7 +13,8 @@
                 'id':elementHtml.attr('id'),
                 'quantity': 1,
                 'image': elementHtml.attr('image'),
-                'perfume': elementHtml.attr('perfume')
+                'perfume': elementHtml.attr('perfume'),
+                'price': elementHtml.attr('price')
             };
 
         addItemToLocal(product);
@@ -68,5 +69,79 @@
 
     }
     cartNumberDisplay();
+
+    // ------------------------------ script carrinho 2-------------------------------------//
+
+    function confericar(){
+        let cartItem = JSON.parse(localStorage.getItem('prdInCart'))
+        if (cartItem === null){
+
+        }else {
+            var carttr = $(".carttr");
+            var htmltr = '';
+            var total = 0;
+            var totalsub = 0;
+            cartItem.forEach(item =>{
+                var price = 'R$ '+ item.price;
+                var totaltr = parseInt(item.price) * parseInt(item.quantity);
+                totalsub += totaltr;
+                htmltr += "" +
+            "<tr class='cart_table_item'>"+
+                    "<td class='product-thumbnail'>" +
+                            "<div class='product-thumbnail-wrapper'>" +
+                                "<a href='#' class='product-thumbnail-remove' title='Remove Product' id='"+item.id+"' onclick='decreasesCart(this);'><i class='fas fa-times'></i></a>" +
+                                "<a href='shop-product-sidebar-right.html' class='product-thumbnail-image' title='Luxury Bag'><img width='90' height='90' alt='' class='img-fluid' src='"+item.image+"'></a>"+
+                            "</div>"+
+                   "</td>"+
+                    "<td class='product-name'><a href='#' class='font-weight-semi-bold text-color-dark text-color-hover-primary text-decoration-none'>"+item.perfume+"</a>"+
+                    "</td>"+
+                    "<td class='product-price'><span class='amount font-weight-medium text-color-grey'>"+price+"</span>"+
+                    "</td>"+
+                    "<td class='product-subtotal text-end'><span class='amount text-color-dark font-weight-bold text-4'>"+item.quantity+"</span>"+
+                    "</td>"+
+                    "<td class='product-subtotal text-end'><span class='amount text-color-dark font-weight-bold text-4'>R$ "+totaltr+"</span>"+
+                    "</td>"+
+            "</tr>";
+
+            });
+            carttr.append(htmltr);
+            total = parseInt(totalsub) + 20;
+            if (document.getElementById('totalsub')){
+                document.getElementById('totalsub').innerHTML=totalsub;
+                document.getElementById('total').innerHTML=total;
+            }
+
+        }
+
+    }
+    confericar();
+
+    function decreasesCart(element){
+        event.preventDefault();
+        let elementHTML = $(element);
+        let cartItem = JSON.parse(localStorage.getItem('prdInCart'))
+        console.log(cartItem)
+        cartItem.forEach(item => {
+           if (item.id == elementHTML.attr('id')){
+
+           }else {
+               products.push(item)
+           }
+            localStorage.setItem('prdInCart', JSON.stringify(products));
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: 'Produto deletado do carrinho',
+                showConfirmButton: false,
+                timer: 1500
+            })
+            setTimeout(function (){
+                window.location.reload()
+            },1500);
+
+        });
+
+
+    }
 
 </script>
